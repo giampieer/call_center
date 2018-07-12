1c98f805ac4ab7647bbfdf211a8a42acd56393ac
giampieer <giampieer24@gmail.com>
2018-07-12 01:10:08 -0500

    validation static

--- src/pages/ip/ip.ts
+++ src/pages/ip/ip.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {AlertController, NavController} from 'ionic-angular';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {TabsPage} from "../tabs/tabs";
 
@@ -10,17 +10,31 @@ import {TabsPage} from "../tabs/tabs";
 export class IpPage {
   FrmIp: FormGroup;
   ipData : any;
-  constructor(public navCtrl: NavController, public formBuilder: FormBuilder) {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController, public formBuilder: FormBuilder) {
     this.ipData = JSON.parse(localStorage.getItem('ip'));
     if(window.localStorage.getItem('ip')){
       this.navCtrl.setRoot(TabsPage);
     }
     this.FrmIp = formBuilder.group({
-      ip: ['', Validators.compose([Validators.required])],
+      id: ['', Validators.compose([Validators.required])],
+      pass: ['', Validators.compose([Validators.required])],
+      ip: ['', Validators.compose([Validators.required])]
     });
   }
   resgisterIp() {
     localStorage.setItem('ip', JSON.stringify(this.FrmIp.value.ip));
-    this.navCtrl.setRoot(TabsPage);
+    if (this.FrmIp.value.id === "giampieer24" && this.FrmIp.value.pass === "1111" ) {
+      this.navCtrl.setRoot(TabsPage);
+    } else {
+      this.alert("Id o pass incorrectos")
+    }
+  }
+  alert(mensaje: string) {
+    let alert = this.alertCtrl.create({
+      title: 'Información',
+      subTitle: mensaje,
+      buttons: ['Cerrar']
+    });
+    alert.present();
   }
 }
